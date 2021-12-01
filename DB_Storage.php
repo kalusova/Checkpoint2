c37cef5c3af6bdeb8358502d12eff95ef090637a
Katarína Kalusová <kallusova.katka@gmail.com>
2021-12-01 09:38:28 +0100

    initial commit new repository

---
+++ DB_Storage.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+include "Order.php";
+
+class DB_Storage
+{
+    public $mysqli;
+
+    /**
+     * DB_Storage constructor.
+     * @param mysqli $mysqli
+     */
+    public function __construct(mysqli $mysqli)
+    {
+        $this->mysqli = $mysqli;
+    }
+
+    /**
+     * @return Order[]
+     */
+    public function getAll() : array {
+        $query = "SELECT * FROM Orders";
+        $orders = [];
+        if($result = $this->mysqli->query($query)){
+            while($row = $result->fetch_row() ){
+                $order = new Order($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+                $orders[] = $order;
+            }
+        }
+        return $orders;
+
+    }
+    public function saveOrder(Order $order) : void {
+        $sql = "INSERT INTO Orders (meno, priezvisko, start, end, state)
+                VALUES ('$order->name', '$order->surname', '$order->start', '$order->end', '$order->state')";
+
+        if ($this->mysqli->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $this->mysqli->error;
+        }
+    }
+    public function createOrder(string $name, string $surname, string $start, string $state) : void {
+        $sql = "INSERT INTO Orders (meno, priezvisko, start, state)
+                VALUES ('$name', '$surname', '$start', '$state')";
+        if ($this->mysqli->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $this->mysqli->error;
+        }
+    }
+
+    public function deleteRow(int $id) : void
+    {
+        $sql = "DELETE FROM Orders WHERE id=$id";
+        if ($this->mysqli->query($sql) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . $this->mysqli->error;
+        }
+    }
+
+    public function editState($id, $state) : void
+    {
+        $sql = "UPDATE Orders SET state='$state' WHERE id=$id";
+        if ($this->mysqli->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $this->mysqli->error;
+        }
+    }
+    public function editEnd($id, $end) : void
+    {
+        $sql = "UPDATE Orders SET end='$end', state='Closed' WHERE id=$id";
+        if ($this->mysqli->query($sql) === TRUE) {
+            //echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $this->mysqli->error;
+        }
+    }
+}
---
+++ check.php
@@ -0,0 +1,51 @@
+<?php
+ob_start();
+ini_set('display_errors', 1);
+include 'db_connect.php';
+
+$meno_login=$_POST["u_name"];
+$heslo_login=$_POST["psw"];
+$login_ok = 0;
+
+$sql = "SELECT count(*) as pocet FROM users where login='".$meno_login."' and pass='".$heslo_login."'";
+
+if ($result = $mysqli -> query($sql)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $login_ok=$row["pocet"];
+    }
+}
+
+$result -> free_result();
+$mysqli -> close();
+
+if ($login_ok==1) {
+    setcookie("LoginOK", "1", time()+3600);
+}
+else {
+    setcookie("LoginOK", "0", time()+3600);
+}
+
+?>
+
+<html lang="utf8">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<body>
+<?php
+if ($login_ok==1) {
+    ?>
+    <SCRIPT language="JavaScript">
+        window.location="dashboard.php";
+    </SCRIPT>
+<?php
+}
+else {
+?>
+    <SCRIPT language="JavaScript">
+        window.location="login.html";
+    </SCRIPT>
+    <?php
+}
+ob_end_flush();
+?>
+</body>
+</html>
