bd65ebb7fb391c5a6fc3d30a4ef1ae64608621ca
Katarína Kalusová <kallusova.katka@gmail.com>
2022-01-17 18:25:25 +0100

    kontrola na strane servera na zadavane hodnoty

--- database/register_save.php
+++ database/register_save.php
@@ -1,7 +1,4 @@
 <?php
- 
-//echo "ŠTARTUJEM!";
- 
 require_once "db_connect.php";
 require_once "DB_Storage.php";
  
@@ -11,13 +8,27 @@ $priezvisko_val = $_POST["priezvisko"];
 $login_val = $_POST["login"];
 $passwd_val = $_POST["passwd"];
 $email_val = $_POST["email"];
- 
-// TO-DO: pridať kontrolu na strane servera na formáty
- 
 
-// vytvorím inštanciu
-$storage = new DB_Storage($mysqli);
- 
-$storage->newCustomer($meno_val, $priezvisko_val, $login_val, $passwd_val, $email_val);
 
-?>
+        if(!preg_match("/^[a-zA-Z0-9]+$/",$login_val)){ // check username allowed capital and small letters
+            echo '<script>alert("Pouzivatelske meno musi obsahovat iba male / velke pismena a cislice od 0-9!!")</script>';
+        }
+        else if(!preg_match("/^[a-zA-Z]+$/",$meno_val)){
+            echo '<script>alert("Meno prosim bez diakritiky!!")</script>';
+        }
+        else if(!preg_match("/^[a-zA-Z]+$/",$priezvisko_val)){
+            echo '<script>alert("Priezvisko prosim bez diakritiky!!")</script>';
+        }
+        else if($email_val == ''){ //check email not empty
+            echo '<script>alert("Prosim zadaj email")</script>';
+        }
+        else if(!filter_var($email_val, FILTER_VALIDATE_EMAIL)){ //check valid email format
+            echo '<script>alert("Prosim zadaj korektnu emailovu adresu !!")</script>';
+        }
+        else if($passwd_val == ''){ //check password not empty
+            echo '<script>alert("Prosim zadaj heslo")</script>';
+        }
+        else{
+            $storage = new DB_Storage($mysqli);
+            $storage->newCustomer($meno_val, $priezvisko_val, $login_val, $passwd_val, $email_val);
+        }
